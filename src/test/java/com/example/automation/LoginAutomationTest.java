2d74042af45e4481b26655417cecd20670cb81e6
Shama Anjum <98334746+yugen-21@users.noreply.github.com>
2024-12-31 20:17:34 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -15,7 +15,7 @@ public class LoginAutomationTest {
     @Test
     public void testLogin() {
         // Set up ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shama\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         try {
